1daa65d8ac08f2409c81e17a93f37928361ef9c2
Dary <Dafna_pelmeshka@mail.ru>
2023-04-09 14:52:28 +0300

    Code reformate

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate(double weight,double height) {
+    public int calculate(double weight, double height) {
         double height2 = Math.pow(height, 2);
         double bmi = weight / height2;
         return (int) bmi;
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         double weight = 60.5; // вес в кг
         double height = 1.60; //рост в метрах
-        int bmi = service.calculate(weight,height);
+        int bmi = service.calculate(weight, height);
         System.out.println("Индекс массы тела = " + bmi);
 
     }
